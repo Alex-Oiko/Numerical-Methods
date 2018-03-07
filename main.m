fa2bfc1f7aecad2a52e351c3820b434c4d32c083
King <kingwongf@gmail.com>
2018-03-07 15:44:20 +0000

    math

--- main.m
+++ main.m
@@ -28,44 +28,44 @@ S0 = 100; r = 0.04; sigma = 0.3; K = [90 100 110]; T = 1; n = [4 12 50]; M = 1E5
 %    continuous_monitoring_approx(i) = expectationContinuousMonitoringApprox(S0,K(2),sigma,r,T,2^i);
 %end
 
-%% Question 4 Price as Control Variate
-%geo_simulations = [];
-%lb_simulations = [];
-%epsilon = [];
-
-%for i=1:size(n,2)
-%    for k = 1:size(K,2)
-%        dt = T/n(i); 
-
-%        [correlcoef_lb, bstarhat_lb,correlcoef_geo, bstarhat_geo] = controlVariateCoefficients(S0, K(k), sigma, r, T, dt, M);
-        
-%        [lb_std, lb_asian_price, lb_conf_int,lb_exec_time] = asianMCCtrVarLB(S0, K(k), sigma, r, T, dt, n(i), M, bstarhat_lb, alpha);
-%        [geo_std, geo_asian_price, geo_conf_int,geo_exec_time] = asianMCCtrVarGeo(S0, K(k), sigma, r, T, dt, n(i), M, bstarhat_geo, alpha);
-        
-%        lb_simulations(i,k,:) = [lb_std, lb_asian_price, lb_conf_int];
-%        geo_simulations(i,k,:) = [geo_std, geo_asian_price, geo_conf_int];
-        
-%        epsilon(i,k) = (lb_exec_time*lb_std^2)/(geo_exec_time*geo_std^2);
-%    end
-%end
-
-% Question 4 Delta as Control Variate
-geo_delta_simulations = [];
-lb_delta_simulations = [];
+% Question 4 Price as Control Variate
+geo_simulations = [];
+lb_simulations = [];
 epsilon = [];
 
 for i=1:size(n,2)
    for k = 1:size(K,2)
        dt = T/n(i); 
 
-       [correlcoef_lb_delta, bstarhat_lb_delta,correlcoef_geo_delta, bstarhat_geo_delta] = Delta_CV_Coeff(S0, K(k), sigma, r, T, dt, M);
+       [correlcoef_lb, bstarhat_lb,correlcoef_geo, bstarhat_geo] = controlVariateCoefficients(S0, K(k), sigma, r, T, dt, M);
         
-       [lb_std_delta, lb_asian_delta, lb_conf_int_delta,lb_exec_delta_time] = asianMCCtrVarLBDelta(S0, K(k), sigma, r, T, dt, n(i), M, bstarhat_lb_delta, alpha);
-       [geo_std_delta, geo_asian_delta, geo_conf_int_delta,geo_exec_delta_time] = asianMCCtrVarGeoDelta(S0, K(k), sigma, r, T, dt, n(i), M, bstarhat_geo_delta, alpha);
+       [lb_std, lb_asian_price, lb_conf_int,lb_exec_time] = asianMCCtrVarLB(S0, K(k), sigma, r, T, dt, n(i), M, bstarhat_lb, alpha);
+       [geo_std, geo_asian_price, geo_conf_int,geo_exec_time] = asianMCCtrVarGeo(S0, K(k), sigma, r, T, dt, n(i), M, bstarhat_geo, alpha);
         
-       lb_delta_simulations(i,k,:) = [lb_std_delta, lb_asian_delta, lb_conf_int_delta];
-       geo_delta_simulations(i,k,:) = [geo_std_delta, geo_asian_delta, geo_conf_int_delta];
+       lb_simulations(i,k,:) = [lb_std, lb_asian_price, lb_conf_int];
+       geo_simulations(i,k,:) = [geo_std, geo_asian_price, geo_conf_int];
         
-       epsilon_delta(i,k) = (lb_exec_delta_time*lb_std_delta^2)/(geo_exec_delta_time*geo_std_delta^2);
+       epsilon(i,k) = (lb_exec_time*lb_std^2)/(geo_exec_time*geo_std^2);
    end
 end
+
+% % Question 4 Delta as Control Variate
+% geo_delta_simulations = [];
+% lb_delta_simulations = [];
+% epsilon = [];
+% 
+% for i=1:size(n,2)
+%    for k = 1:size(K,2)
+%        dt = T/n(i); 
+% 
+%        [correlcoef_lb_delta, bstarhat_lb_delta,correlcoef_geo_delta, bstarhat_geo_delta] = Delta_CV_Coeff(S0, K(k), sigma, r, T, dt, M);
+%         
+%        [lb_std_delta, lb_asian_delta, lb_conf_int_delta,lb_exec_delta_time] = asianMCCtrVarLBDelta(S0, K(k), sigma, r, T, dt, n(i), M, bstarhat_lb_delta, alpha);
+%        [geo_std_delta, geo_asian_delta, geo_conf_int_delta,geo_exec_delta_time] = asianMCCtrVarGeoDelta(S0, K(k), sigma, r, T, dt, n(i), M, bstarhat_geo_delta, alpha);
+%         
+%        lb_delta_simulations(i,k,:) = [lb_std_delta, lb_asian_delta, lb_conf_int_delta];
+%        geo_delta_simulations(i,k,:) = [geo_std_delta, geo_asian_delta, geo_conf_int_delta];
+%         
+%        epsilon_delta(i,k) = (lb_exec_delta_time*lb_std_delta^2)/(geo_exec_delta_time*geo_std_delta^2);
+%    end
+% end
